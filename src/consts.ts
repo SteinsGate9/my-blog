e5bf68012b4359707f76f635b2c7c98b299e0fd1
Shicheng Huang <shichenghuang@Ss-MacBook-Air.local>
2026-06-11 20:21:28 -0700

    initial blog setup

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Shicheng Huang';
+export const SITE_DESCRIPTION = 'Personal blog — thoughts on investing, life, and building things.';
